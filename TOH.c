031eb661c7d864b72195cb29c857823f8b46340d
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-07-24 13:23:11 +0530

    TOH complete

---
+++ TOH.c
@@ -0,0 +1,37 @@
+//Tower Of Hanoi Problem
+//3-->pegs  
+//many number of disks(n)
+
+//Algorithm
+/*
+Toh(n,src,dest,aux)
+if(n=1)
+move disk from source to destination
+else
+TOH(n-1,src,aux,dest)
+move disk from source to destination
+TOH(n-1,aux,dest,src)
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void TOH(int,char,char,char);
+
+void main(){
+    int n;
+    printf("\nEnter the number of disks:\t");
+    scanf("%d",&n);
+    TOH(n,'A','C','B');
+}
+
+void TOH(int n,char src,char dest,char aux){
+    if(n==1){
+        printf("\nmove disk 1 from %c to %c",src,dest);
+    }
+    else{
+        TOH(n-1,src,aux,dest);
+        printf("\nmove disk %d from %c to %c",n,src,dest);
+        TOH(n-1,aux,dest,src);
+    }
+}
