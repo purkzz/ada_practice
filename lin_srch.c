a40ffa388508363bb55dc220ada8c400521f0878
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-07-25 19:50:12 +0530

    update

--- lin_srch.c
+++ lin_srch.c
@@ -5,22 +5,29 @@ Time Complexity-->O(n)
 you search linearly
 */
 
+//Linear search algorithm
+/*
+Time Complexity-->O(n)
+
+you search linearly
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-int lin_search(int [],int,int);
-int a[10000000],n=10,key,status=0;
+int lin_search(int [],int,int,int);
+int a[10000],n=1000,key,status=0;
 
 void main(){
     clock_t start,end;
-    while(n!=1000000){
+    while(n<=10000){
         for(int i=0;i<n;i++){a[i]=rand();}        //assigning random numbers to the array
         key=a[n-1];
         
         start=clock();                            //starting the timer
-        status=lin_search(a,n,key);               //calling the function
-        for(int j=0;j<50000;j++){int temp=9/89;}  //producing delay
+        status=lin_search(a,0,n,key);               //calling the function
+        for(int j=0;j<5000000;j++){int temp=9/89;}  //producing delay
         end=clock();                              //end the time
 
         if(status!=-1){
@@ -29,11 +36,12 @@ void main(){
         n+=1000;
     }
 }
-int lin_search(int a[],int size,int key){
-    for(int i=0;i<size;i++){
-        if(a[i]==key){
-            return 1;
-        }
-    }
-    return -1;
+int lin_search(int a[],int i,int size,int key){
+    if(i>size)
+        return -1;
+    else if(a[i]==key)
+        return i;
+
+    else
+        return lin_search(a,i+1,size,key);    
 }
